7e12e50bf764b590f05b6a7500dc2a7acd545a76
wumingcp <wumingcp@hotmail.com>
2018-08-28 14:31:50 +0800

    Add the script to transfer log codes to cosmos DB

---
+++ TrasferLogCodesToCosmosDBScript.ps1
@@ -0,0 +1,10 @@
+﻿$PackageName=nuget.exe list Microsoft.OpenPublishing.LogCodeService.LogCodeToCosmosDB  -Source https://www.myget.org/F/op-dev/api/v3/index.json -NonInteractive -PreRelease
+$PackageName=$PackageName.Replace(" ", ".")
+
+echo "Install LogCodeToCosmosDB"
+nuget.exe install Microsoft.OpenPublishing.LogCodeService.LogCodeToCosmosDB  -Source https://www.myget.org/F/op-dev/api/v3/index.json -NonInteractive -PreRelease
+
+$ToolName=[string]::Format("{0}\{1}\lib\netcoreapp2.1\Microsoft.OpenPublishing.LogCodeService.LogCodeToCosmosDB.exe", (Get-Location).Path, $PackageName)
+
+echo "Upload LogCodes to CosmosDB"
+iex "& $ToolName -f $(FolderPath) -e $(Endpoint) -a $(Authorization) -d $(DatabaseId) -c $(CollectionId)"
